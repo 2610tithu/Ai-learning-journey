ecea87fb312c46b8cefd30cce17879c55ca37f11
2610tithu <kalthiyatilak1@gmail.com>
2025-05-18 16:53:38 +0530

    Add day  1 python basics

---
+++ day1_python_basics (2).py
@@ -0,0 +1,31 @@
+
+# Day 1: Python Basics – Variables, Data Types, Input/Output
+
+# Variables and Data Types
+name = "Prachi"
+age = 22
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+# Type Conversion
+age_str = str(age)
+pi = float("3.14")
+
+# Input and Output
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+fav_number = input("Enter your favorite number: ")
+
+print(f"Hello {user_name}! You are {user_age} years old and your favorite number is {fav_number}.")
+
+# Operators
+a = 10
+b = 3
+print("Addition:", a + b)
+print("Floor Division:", a // b)
+print("Exponent:", a ** b)
